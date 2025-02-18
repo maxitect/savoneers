4874cb2579dcdc6df9f924bd8cad8664e9576100
maxitect <maxime.downe@gmail.com>
2025-02-18 22:54:06 +0000

    chore: update year in Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-8 text-center">
-          <p>&copy; 2024 Savoneers. All rights reserved.</p>
+          <p>&copy; 2025 Savoneers. All rights reserved.</p>
         </div>
       </div>
     </footer>
